e1fa18fa4f9af524aebb818d0a2270720d365aaa
Dino Cyrano Dabon <160745103+Dinodino1231@users.noreply.github.com>
2024-06-30 00:37:31 +0800

    Update addsalesreport.php

--- addsalesreport.php
+++ addsalesreport.php
@@ -13,12 +13,17 @@
                 <div>
                     <button>CLOSE</button>
                     <form method='get'>
-                        <select name='addToReport'>";
+                        <select name='addToReport0'>";
                             for($x = 0; $x < sizeof($row); $x++){
                                 echo "<option value='".$row[$x]['product_ID']."'>".$row[$x]['product_ID'] . " " . $row[$x]['product_name']."</option>";
                             }
             echo "      </select>
-                        <input type='submit' value='Add To Report'>
+                        <select name='addToReport1'>";
+                            for($x = 0; $x < sizeof($row); $x++){
+                                echo "<option value='".$row[$x]['product_ID']."'>".$row[$x]['product_ID'] . " " . $row[$x]['product_name']."</option>";
+                            }
+            echo "      </select>
+                        <input type='submit' value='Add To Report' name='addToReport'>
                     </form>
                 </div>
             ";
@@ -32,7 +37,41 @@
 
 <?php
     if(isset($_GET["addToReport"])){
-        $productID = $_GET["addToReport"];
+        $productID = $_GET["addToReport0"];
+        $conn = mysqli_connect("localhost","root","","mamaflors");
+        if($conn->connect_error){
+            die("ERROR". $conn->connect_error);
+        }
+        else{
+            $sql = "SELECT * FROM product
+                    WHERE product_ID = $productID";
+            $result = $conn->query($sql);
+            $rowProduct = $result->fetch_all(MYSQLI_ASSOC);
+
+            if(sizeof($rowProduct) > 0){
+                // Automatically Get The Account ID on Session
+                echo "<div>
+                        <form method='post'>
+                            ".$rowProduct[0]['product_name']."
+                            <br>
+                            <input type='hidden' name='productID' value='".$rowProduct[0]['product_ID']."'>
+                            Cooked Quantity <input type='number' name='cookedqty'>
+                            Reheat Quantity <input type='number' name='reheatqty'>
+                            Total Display Quantity <input type='number' name='totaldisplayqty'>
+                            Leftover Quantity <input type='number' name='leftoverqty'>
+                            Total Sold Quantity <input type='number' name='totalsoldqty'>
+                            <input type='submit' value='Submit' name='submitReport'>
+                        </form>
+                    </div>
+                ";
+            }
+            else{
+                echo "Invalid Input Please Add a Branch or Product";
+            }
+        }
+        $conn->close();
+
+        $productID = $_GET["addToReport1"];
         $conn = mysqli_connect("localhost","root","","mamaflors");
         if($conn->connect_error){
             die("ERROR". $conn->connect_error);
