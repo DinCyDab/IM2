8ce32f4aba69e9e7bf2fd8a45ba680c501d5b8cb
Dino Cyrano Dabon <160745103+Dinodino1231@users.noreply.github.com>
2024-07-07 22:04:20 +0800

    updated DB table

--- mamaflors (2).sql
+++ mamaflors (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 04, 2024 at 11:17 AM
+-- Generation Time: Jul 07, 2024 at 04:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,7 +31,7 @@ CREATE TABLE `account` (
   `account_ID` varchar(20) NOT NULL,
   `created_date` date DEFAULT curdate(),
   `created_time` time DEFAULT curtime(),
-  `password` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `role` enum('Regular','Administrator') DEFAULT 'Regular',
   `account_status` enum('Active','Inactive') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,7 +42,9 @@ CREATE TABLE `account` (
 
 INSERT INTO `account` (`account_ID`, `created_date`, `created_time`, `password`, `role`, `account_status`) VALUES
 ('0001', '2024-06-28', '19:42:05', '$2y$10$IF7UGA69sIwxQ2Auc6W0BuTCdrnMT1KEE2qtZc3FMFY', 'Regular', 'Active'),
-('0002', '2024-06-28', '19:47:58', '$2y$10$jfveeJ45dgeVJ9E.fg.7Gu0SOA9iBkE2O8.VcpzwU3W', 'Regular', 'Active');
+('0002', '2024-06-28', '19:47:58', '$2y$10$jfveeJ45dgeVJ9E.fg.7Gu0SOA9iBkE2O8.VcpzwU3W', 'Regular', 'Active'),
+('0004', '2024-07-04', '19:44:16', '$2y$10$WPC1fR19L0lk1mKuzCzCqeaau654f7Tnqt5kt5.qclvTsYL8kjMqS', 'Regular', 'Active'),
+('0005', '2024-07-05', '10:03:07', '$2y$10$Dy4WnIfKDI/8u5ibcpR0Hewjm5dYat9PwTUrQ08mV4BBuabp/T2VS', 'Administrator', 'Active');
 
 -- --------------------------------------------------------
 
@@ -55,8 +57,6 @@ CREATE TABLE `assignment` (
   `branch_ID` int(11) DEFAULT NULL,
   `staff_ID` varchar(20) NOT NULL,
   `assignment_date` date DEFAULT NULL,
-  `time_in` time DEFAULT NULL,
-  `time_out` time DEFAULT NULL,
   `note` varchar(100) DEFAULT NULL,
   `assignment_status` enum('Present','Absent') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -65,17 +65,62 @@ CREATE TABLE `assignment` (
 -- Dumping data for table `assignment`
 --
 
-INSERT INTO `assignment` (`assignment_ID`, `branch_ID`, `staff_ID`, `assignment_date`, `time_in`, `time_out`, `note`, `assignment_status`) VALUES
-(3, 3, '0003', '2024-07-04', '00:00:00', '00:00:00', '', 'Present'),
-(9, 3, '0002', '2024-07-04', '16:16:00', '04:16:00', 'late', 'Present'),
-(17, 3, '0001', '2024-07-03', '08:57:00', '16:57:00', 'Too early', 'Present'),
-(18, NULL, '0002', '2024-07-03', NULL, NULL, NULL, NULL),
-(19, NULL, '0003', '2024-07-03', NULL, NULL, NULL, NULL),
-(20, NULL, '0001', '2024-07-05', NULL, NULL, NULL, NULL),
-(21, NULL, '0003', '2024-07-05', NULL, NULL, NULL, NULL),
-(22, 1, '0001', '2024-07-12', '00:00:00', '00:00:00', '', 'Present'),
-(23, NULL, '0002', '2024-07-12', NULL, NULL, NULL, NULL),
-(24, NULL, '0003', '2024-07-12', NULL, NULL, NULL, NULL);
+INSERT INTO `assignment` (`assignment_ID`, `branch_ID`, `staff_ID`, `assignment_date`, `note`, `assignment_status`) VALUES
+(3, 3, '0003', '2024-07-04', '', 'Present'),
+(9, 1, '0002', '2024-07-04', 'late', 'Absent'),
+(17, 3, '0001', '2024-07-03', 'Too early', 'Present'),
+(18, NULL, '0002', '2024-07-03', NULL, NULL),
+(19, NULL, '0003', '2024-07-03', NULL, NULL),
+(22, 1, '0001', '2024-07-12', '', 'Present'),
+(23, NULL, '0002', '2024-07-12', NULL, NULL),
+(24, NULL, '0003', '2024-07-12', NULL, NULL),
+(29, NULL, '0001', '2024-07-05', NULL, NULL),
+(30, NULL, '0002', '2024-07-05', NULL, NULL),
+(31, 1, '0003', '2024-07-05', 'too early for time in', 'Present'),
+(33, 3, '0005', '2024-07-05', '', 'Present'),
+(34, 1, '0001', '2024-07-06', '', 'Present'),
+(35, 3, '0002', '2024-07-06', '', 'Present'),
+(36, NULL, '0003', '2024-07-06', NULL, NULL),
+(38, 1, '0005', '2024-07-06', '', 'Present'),
+(39, 3, '0001', '2024-07-07', '', 'Present'),
+(40, NULL, '0002', '2024-07-07', NULL, NULL),
+(41, NULL, '0003', '2024-07-07', NULL, NULL),
+(43, NULL, '0005', '2024-07-07', NULL, NULL),
+(44, 3, '0001', '2024-08-08', NULL, NULL),
+(45, 1, '0002', '2024-08-08', NULL, NULL),
+(46, NULL, '0003', '2024-08-08', NULL, NULL),
+(47, NULL, '0004', '2024-08-08', NULL, NULL),
+(48, NULL, '0005', '2024-08-08', NULL, NULL),
+(49, NULL, '0001', '2024-07-08', NULL, NULL),
+(50, NULL, '0002', '2024-07-08', NULL, NULL),
+(51, NULL, '0003', '2024-07-08', NULL, NULL),
+(53, NULL, '0005', '2024-07-08', NULL, NULL),
+(59, NULL, '0001', '2024-07-10', NULL, NULL),
+(60, NULL, '0002', '2024-07-10', NULL, NULL),
+(61, NULL, '0003', '2024-07-10', NULL, NULL),
+(63, NULL, '0005', '2024-07-10', NULL, NULL),
+(64, NULL, '0001', '2024-07-11', NULL, NULL),
+(65, NULL, '0002', '2024-07-11', NULL, NULL),
+(66, NULL, '0003', '2024-07-11', NULL, NULL),
+(67, 3, '0004', '2024-07-11', '', 'Present'),
+(68, NULL, '0005', '2024-07-11', NULL, NULL),
+(69, NULL, '0001', '2024-07-01', NULL, NULL),
+(70, NULL, '0002', '2024-07-01', NULL, NULL),
+(71, NULL, '0003', '2024-07-01', NULL, NULL),
+(72, 1, '0004', '2024-07-01', '', 'Present'),
+(73, NULL, '0005', '2024-07-01', NULL, NULL),
+(74, NULL, '0001', '2024-07-02', NULL, NULL),
+(75, NULL, '0002', '2024-07-02', NULL, NULL),
+(76, NULL, '0003', '2024-07-02', NULL, NULL),
+(77, 1, '0004', '2024-07-02', '', 'Present'),
+(78, NULL, '0005', '2024-07-02', NULL, NULL),
+(82, 3, '0004', '2024-07-03', '', 'Present'),
+(83, NULL, '0005', '2024-07-03', NULL, NULL),
+(84, NULL, '0001', '2024-07-04', NULL, NULL),
+(85, NULL, '0002', '2024-07-04', NULL, NULL),
+(86, NULL, '0003', '2024-07-04', NULL, NULL),
+(87, 1, '0004', '2024-07-04', '', 'Present'),
+(88, NULL, '0005', '2024-07-04', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -101,8 +146,8 @@ CREATE TABLE `branch` (
 --
 
 INSERT INTO `branch` (`branch_ID`, `branch_name`, `established_date`, `street_name`, `barangay`, `city`, `province`, `postal_code`, `contact_number`, `branch_status`) VALUES
-(1, 'tipolo', '2023-01-24', 'Tipolo Street', 'Maguikay', 'Mandaue', 'Cebu', '6014', '09123456789', 'Active'),
-(3, 'Test Branch', '2020-02-04', '', '', '', '', '', '', 'Active');
+(1, 'Tipolos', '2023-01-24', 'Tipolo Street', 'Maguikay', 'Mandaue', 'Cebu', '6014', '09123456789', 'Active'),
+(3, 'Lapu Lapu', '2020-02-04', '', '', '', '', '', '', 'Active');
 
 -- --------------------------------------------------------
 
@@ -123,9 +168,9 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_ID`, `product_name`, `product_description`, `product_price`, `product_status`) VALUES
-(10, 'Liempoer', 'TESTing', 300.00, 'Active'),
-(11, 'Spring Rolls', 'Spring Roller', 10.00, 'Active'),
-(12, 'Chicken', 'Chickenazor', 250.00, 'Active');
+(10, 'Liempo', 'Baboy na Liemp', 300.00, 'Active'),
+(11, 'Spring Rolls', 'Spring Rolled', 10.00, 'Active'),
+(12, 'Chicken', 'Gi luto na Chicken', 270.00, 'Active');
 
 -- --------------------------------------------------------
 
@@ -152,17 +197,12 @@ CREATE TABLE `salesreport` (
 --
 
 INSERT INTO `salesreport` (`report_ID`, `account_ID`, `branch_ID`, `product_ID`, `report_date`, `report_time`, `cooked_qty`, `reheat_qty`, `total_display_qty`, `left_over_qty`, `total_sold_qty`) VALUES
-(9, '0001', 1, 10, '2024-06-30', '14:50:57', 5, 5, 5, 5, 5),
-(10, '0001', 1, 11, '2024-06-30', '14:50:57', 5, 5, 5, 5, 5),
-(11, '0001', 1, 12, '2024-06-30', '14:50:57', 5, 5, 5, 5, 5),
-(12, '0001', 1, 11, '2024-06-30', '15:13:50', 20, 20, 20, 2, 2),
-(13, '0001', 1, 12, '2024-06-30', '15:13:50', 2, 2, 2, 2, 2),
-(14, '0001', 1, 10, '2024-06-30', '15:15:21', 5, 5, 5, 5, 5),
-(15, '0001', 1, 10, '2024-06-30', '15:15:43', 5, 5, 5, 5, 5),
-(16, '0002', 3, 10, '2024-06-30', '15:37:01', 40, 2, 2, 2, 2),
-(17, '0002', 3, 10, '2024-06-30', '19:21:08', 5, 5, 5, 5, 5),
-(18, '0002', 3, 11, '2024-06-30', '19:21:08', 5, 5, 5, 5, 5),
-(19, '0002', 3, 12, '2024-06-30', '19:21:08', 5, 55, 5, 5, 5);
+(26, '0004', 1, 10, '2024-06-20', '18:15:43', 20, 10, 10, 10, 15),
+(27, '0004', 1, 12, '2024-07-05', '18:15:43', 50, 10, 10, 10, 40),
+(28, '0005', 3, 10, '2024-07-05', '19:08:23', 5, 5, 5, 5, 5),
+(29, '0005', 3, 11, '2024-07-05', '19:08:23', 50, 0, 50, 0, 50),
+(30, '0005', 3, 10, '2024-07-06', '07:24:12', 20, 0, 20, 5, 15),
+(31, '0005', 3, 11, '2023-07-27', '07:24:12', 100, 20, 200, 5, 220);
 
 -- --------------------------------------------------------
 
@@ -201,7 +241,9 @@ CREATE TABLE `staff` (
 INSERT INTO `staff` (`staff_ID`, `last_name`, `first_name`, `middle_name`, `house_number`, `street_name`, `barangay`, `city`, `province`, `postal_code`, `birth_date`, `gender`, `contact_1`, `contact_2`, `email`, `SSN`, `TIN`, `position_title`, `start_date`, `salary`, `status`) VALUES
 ('0001', 'Dabon', 'Dino Cyrano', 'Azucenas', '628', 'M.L. Quezon', 'Maguikay', 'Mandaue', 'Cebu', '6014', '2000-09-24', 'Male', '09955430188', '09323995999', 'dcdabon1231@gmail.com', '', '', 'Production', '2024-06-28', 528.00, 'Active'),
 ('0002', 'Test', 'Testing', 'Test', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active'),
-('0003', 'test', 'test', 'test', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active');
+('0003', 'test', 'test', 'test', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active'),
+('0004', 'testing', 'testing', 'testing', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active'),
+('0005', 'dab', 'dab', 'd', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active');
 
 --
 -- Indexes for dumped tables
@@ -256,7 +298,7 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `assignment`
 --
 ALTER TABLE `assignment`
-  MODIFY `assignment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `assignment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT for table `branch`
@@ -274,7 +316,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `salesreport`
 --
 ALTER TABLE `salesreport`
-  MODIFY `report_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `report_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- Constraints for dumped tables
