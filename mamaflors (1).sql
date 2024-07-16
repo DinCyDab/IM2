ecf4a6395d6656dfe60fbda9972e3ee9563b3ee4
Dino Cyrano Dabon <160745103+Dinodino1231@users.noreply.github.com>
2024-07-16 13:58:06 +0800

    Update mamaflors (1).sql

--- mamaflors (1).sql
+++ mamaflors (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2024 at 08:05 AM
+-- Generation Time: Jul 16, 2024 at 07:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,7 +32,7 @@ CREATE TABLE `account` (
   `created_date` date DEFAULT curdate(),
   `created_time` time DEFAULT curtime(),
   `password` varchar(100) NOT NULL,
-  `role` enum('Regular','Administrator') DEFAULT 'Regular',
+  `role` enum('Administrator','Owner','Regular') DEFAULT 'Regular',
   `account_status` enum('Active','Inactive') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,10 +41,10 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`account_ID`, `created_date`, `created_time`, `password`, `role`, `account_status`) VALUES
-('0001', '2024-06-28', '19:42:05', '$2y$10$IF7UGA69sIwxQ2Auc6W0BuTCdrnMT1KEE2qtZc3FMFY', 'Regular', 'Active'),
-('0002', '2024-06-28', '19:47:58', '$2y$10$jfveeJ45dgeVJ9E.fg.7Gu0SOA9iBkE2O8.VcpzwU3W', 'Regular', 'Active'),
-('0004', '2024-07-04', '19:44:16', '$2y$10$nlHDg1IYEcmfixnt6l4YaODLsWBgvRY/iPvU8TsPFqoSt.VRG2K/6', 'Regular', 'Active'),
-('0005', '2024-07-05', '10:03:07', '$2y$10$Dy4WnIfKDI/8u5ibcpR0Hewjm5dYat9PwTUrQ08mV4BBuabp/T2VS', 'Administrator', 'Active');
+('0001', '2024-07-16', '08:14:12', '$2y$10$mOl4sZWY.PhrM8pGP7wJge1rNHXO4SOcXvJZNMN6brsb0b7hkgD5K', 'Owner', 'Active'),
+('0002', '2024-07-16', '09:57:07', '$2y$10$cYjVCBrLV6431dYfY2.Ru.2MqfBdfjVtPS0fe4KRUwBiESm4BrtfC', 'Administrator', 'Active'),
+('0003', '2024-07-16', '10:15:38', '$2y$10$hjNoGWB3lKxriXcslXvg8euwA94o.tZcc/IKfLZZuk2oGSKsPQxDW', 'Regular', 'Active'),
+('0004', '2024-07-16', '11:33:43', '$2y$10$BVmbOJUi1ldBqgb51Ie8Ie5yZLYWTrT3RkVP5hi6zgEEnLRrSsl7m', 'Regular', 'Inactive');
 
 -- --------------------------------------------------------
 
@@ -68,104 +68,46 @@ CREATE TABLE `assignment` (
 --
 
 INSERT INTO `assignment` (`assignment_ID`, `branch_ID`, `staff_ID`, `assignment_date`, `note`, `assignment_status`, `time_in`, `time_out`) VALUES
-(3, 3, '0003', '2024-07-04', '', 'Present', NULL, NULL),
-(9, 1, '0002', '2024-07-04', 'late', 'Absent', NULL, NULL),
-(17, 3, '0001', '2024-07-03', 'Too early', 'Present', NULL, NULL),
-(18, NULL, '0002', '2024-07-03', NULL, NULL, NULL, NULL),
-(19, NULL, '0003', '2024-07-03', NULL, NULL, NULL, NULL),
-(29, NULL, '0001', '2024-07-05', NULL, NULL, NULL, NULL),
-(30, NULL, '0002', '2024-07-05', NULL, NULL, NULL, NULL),
-(31, 1, '0003', '2024-07-05', 'too early for time in', 'Present', NULL, NULL),
-(33, 3, '0005', '2024-07-05', '', 'Present', '22:13:37', NULL),
-(34, 1, '0001', '2024-07-06', '', 'Present', NULL, NULL),
-(35, 3, '0002', '2024-07-06', '', 'Present', NULL, NULL),
-(36, NULL, '0003', '2024-07-06', NULL, NULL, NULL, NULL),
-(38, 1, '0005', '2024-07-06', '', 'Present', '22:13:37', NULL),
-(39, 3, '0001', '2024-07-07', '', 'Present', NULL, NULL),
-(40, NULL, '0002', '2024-07-07', NULL, NULL, NULL, NULL),
-(41, NULL, '0003', '2024-07-07', NULL, NULL, NULL, NULL),
-(43, NULL, '0005', '2024-07-07', NULL, NULL, '22:13:37', NULL),
-(44, 3, '0001', '2024-08-08', NULL, NULL, NULL, NULL),
-(45, 1, '0002', '2024-08-08', NULL, NULL, NULL, NULL),
-(46, NULL, '0003', '2024-08-08', NULL, NULL, NULL, NULL),
-(47, NULL, '0004', '2024-08-08', NULL, 'Present', '22:13:33', NULL),
-(48, NULL, '0005', '2024-08-08', NULL, NULL, '22:13:37', NULL),
-(69, NULL, '0001', '2024-07-01', NULL, NULL, NULL, NULL),
-(70, NULL, '0002', '2024-07-01', NULL, NULL, NULL, NULL),
-(71, NULL, '0003', '2024-07-01', NULL, NULL, NULL, NULL),
-(72, 1, '0004', '2024-07-01', '', 'Present', '22:13:33', NULL),
-(73, NULL, '0005', '2024-07-01', NULL, NULL, '22:13:37', NULL),
-(74, NULL, '0001', '2024-07-02', NULL, NULL, NULL, NULL),
-(75, NULL, '0002', '2024-07-02', NULL, NULL, NULL, NULL),
-(76, NULL, '0003', '2024-07-02', NULL, NULL, NULL, NULL),
-(77, 1, '0004', '2024-07-02', '', 'Present', '22:13:33', NULL),
-(78, NULL, '0005', '2024-07-02', NULL, NULL, '22:13:37', NULL),
-(82, 3, '0004', '2024-07-03', '', 'Present', '22:13:33', NULL),
-(83, NULL, '0005', '2024-07-03', NULL, NULL, '22:13:37', NULL),
-(84, NULL, '0001', '2024-07-04', NULL, NULL, NULL, NULL),
-(87, 1, '0004', '2024-07-04', '', 'Present', '22:13:33', NULL),
-(88, NULL, '0005', '2024-07-04', NULL, NULL, '22:13:37', NULL),
-(89, NULL, '0001', '2024-07-29', NULL, NULL, NULL, NULL),
-(90, NULL, '0002', '2024-07-29', NULL, NULL, NULL, NULL),
-(91, NULL, '0003', '2024-07-29', NULL, NULL, NULL, NULL),
-(92, NULL, '0004', '2024-07-29', NULL, 'Present', '22:13:33', NULL),
-(93, NULL, '0005', '2024-07-29', NULL, NULL, '22:13:37', NULL),
-(94, NULL, '0001', '2024-07-30', NULL, NULL, NULL, NULL),
-(95, NULL, '0002', '2024-07-30', NULL, NULL, NULL, NULL),
-(96, NULL, '0003', '2024-07-30', NULL, NULL, NULL, NULL),
-(97, 1, '0004', '2024-07-30', '', 'Absent', '22:13:33', NULL),
-(98, NULL, '0005', '2024-07-30', NULL, NULL, '22:13:37', NULL),
-(99, NULL, '0001', '2024-07-31', NULL, NULL, NULL, NULL),
-(100, NULL, '0002', '2024-07-31', NULL, NULL, NULL, NULL),
-(101, NULL, '0003', '2024-07-31', NULL, NULL, NULL, NULL),
-(102, NULL, '0004', '2024-07-31', NULL, 'Present', '22:13:33', NULL),
-(103, NULL, '0005', '2024-07-31', NULL, NULL, '22:13:37', NULL),
-(104, NULL, '0001', '2024-07-08', NULL, NULL, NULL, NULL),
-(105, NULL, '0002', '2024-07-08', NULL, NULL, NULL, NULL),
-(106, NULL, '0003', '2024-07-08', NULL, NULL, NULL, NULL),
-(107, 1, '0004', '2024-07-08', '', 'Absent', '22:13:33', NULL),
-(108, 3, '0005', '2024-07-08', '', 'Present', '22:13:37', NULL),
-(115, 1, '0004', '2024-07-05', '', 'Present', '22:13:33', NULL),
-(129, NULL, '0001', '2024-07-09', NULL, NULL, NULL, NULL),
-(130, NULL, '0002', '2024-07-09', NULL, NULL, NULL, NULL),
-(131, NULL, '0003', '2024-07-09', NULL, NULL, NULL, NULL),
-(132, 14, '0004', '2024-07-09', '', 'Present', '22:13:33', NULL),
-(133, 14, '0005', '2024-07-09', '', 'Absent', '22:13:37', NULL),
-(134, NULL, '0001', '2024-07-10', NULL, NULL, NULL, NULL),
-(135, NULL, '0002', '2024-07-10', NULL, NULL, NULL, NULL),
-(136, NULL, '0003', '2024-07-10', NULL, NULL, NULL, NULL),
-(137, 1, '0004', '2024-07-10', '', 'Absent', '22:13:33', NULL),
-(138, NULL, '0005', '2024-07-10', NULL, NULL, '22:13:37', NULL),
-(144, NULL, '0001', '2024-07-18', NULL, NULL, NULL, NULL),
-(145, NULL, '0002', '2024-07-18', NULL, NULL, NULL, NULL),
-(146, NULL, '0003', '2024-07-18', NULL, NULL, NULL, NULL),
-(147, NULL, '0004', '2024-07-18', '', '', '22:13:33', NULL),
-(148, NULL, '0005', '2024-07-18', NULL, NULL, '22:13:37', NULL),
-(154, NULL, '0001', '2024-07-11', NULL, 'Absent', NULL, NULL),
-(155, NULL, '0002', '2024-07-11', NULL, 'Absent', NULL, NULL),
-(156, NULL, '0003', '2024-07-11', NULL, 'Absent', NULL, NULL),
-(157, 3, '0004', '2024-07-11', 'Late', 'Present', '22:18:47', NULL),
-(158, NULL, '0005', '2024-07-11', NULL, 'Absent', '22:13:37', NULL),
-(159, NULL, '0001', '2024-07-12', NULL, 'Absent', NULL, NULL),
-(160, NULL, '0002', '2024-07-12', NULL, 'Absent', NULL, NULL),
-(161, NULL, '0003', '2024-07-12', NULL, 'Absent', NULL, NULL),
-(162, 1, '0004', '2024-07-12', '', 'Present', '17:20:37', NULL),
-(163, NULL, '0005', '2024-07-12', NULL, 'Present', '17:21:02', NULL),
-(169, NULL, '0001', '2024-07-14', NULL, 'Absent', NULL, NULL),
-(170, NULL, '0002', '2024-07-14', NULL, 'Absent', NULL, NULL),
-(171, NULL, '0003', '2024-07-14', NULL, 'Absent', NULL, NULL),
-(172, NULL, '0004', '2024-07-14', NULL, 'Absent', NULL, NULL),
-(173, NULL, '0005', '2024-07-14', NULL, 'Absent', NULL, NULL),
-(174, NULL, '0001', '2024-07-15', NULL, 'Absent', NULL, NULL),
-(175, NULL, '0002', '2024-07-15', NULL, 'Absent', NULL, NULL),
-(176, NULL, '0003', '2024-07-15', NULL, 'Absent', NULL, NULL),
-(177, 1, '0004', '2024-07-15', '', 'Present', '09:08:30', NULL),
-(178, NULL, '0005', '2024-07-15', NULL, 'Present', '09:09:58', NULL),
-(179, NULL, '0001', '2024-07-13', NULL, 'Off', NULL, NULL),
-(180, NULL, '0002', '2024-07-13', NULL, 'Off', NULL, NULL),
-(181, NULL, '0003', '2024-07-13', NULL, 'Off', NULL, NULL),
-(182, NULL, '0004', '2024-07-13', NULL, 'Off', NULL, NULL),
-(183, NULL, '0005', '2024-07-13', NULL, 'Off', NULL, NULL);
+(187, NULL, '0001', '2024-07-17', NULL, 'Off', NULL, NULL),
+(188, NULL, '0002', '2024-07-17', NULL, 'Off', NULL, NULL),
+(190, NULL, '0001', '2024-07-18', NULL, 'Off', NULL, NULL),
+(191, NULL, '0002', '2024-07-18', NULL, 'Off', NULL, NULL),
+(192, NULL, '0003', '2024-07-18', NULL, 'Off', NULL, NULL),
+(193, NULL, '0001', '2024-07-16', NULL, 'Off', NULL, NULL),
+(195, 17, '0003', '2024-07-16', '', 'Late', '11:15:25', NULL),
+(217, NULL, '0004', '2024-07-16', NULL, 'Off', NULL, NULL),
+(218, NULL, '0003', '2024-07-17', NULL, 'Off', NULL, NULL),
+(219, NULL, '0004', '2024-07-17', NULL, 'Off', NULL, NULL),
+(220, 18, '0002', '2024-07-16', '', 'Late', '12:40:52', NULL),
+(221, NULL, '0004', '2024-07-18', NULL, 'Off', NULL, NULL),
+(222, NULL, '0001', '2024-07-09', NULL, 'Late', '08:00:24', NULL),
+(223, NULL, '0002', '2024-07-09', NULL, 'Present', '07:02:15', NULL),
+(224, NULL, '0003', '2024-07-09', NULL, 'Present', '07:57:00', NULL),
+(225, NULL, '0004', '2024-07-09', NULL, 'Late', '12:52:00', NULL),
+(226, NULL, '0001', '2024-07-10', NULL, 'Present', '07:00:24', NULL),
+(227, NULL, '0002', '2024-07-10', NULL, 'Present', '08:01:00', NULL),
+(228, NULL, '0003', '2024-07-10', NULL, 'Late', '08:00:24', NULL),
+(229, NULL, '0004', '2024-07-10', NULL, 'Late', '13:00:13', NULL),
+(230, NULL, '0001', '2024-07-11', NULL, 'Absent', NULL, NULL),
+(231, NULL, '0002', '2024-07-11', NULL, 'Present', '07:00:24', NULL),
+(232, NULL, '0003', '2024-07-11', NULL, 'Present', '07:00:24', NULL),
+(233, NULL, '0004', '2024-07-11', NULL, 'Absent', NULL, NULL),
+(234, NULL, '0001', '2024-07-12', NULL, 'Present', '07:00:24', NULL),
+(235, NULL, '0002', '2024-07-12', NULL, 'Present', '07:00:24', NULL),
+(236, NULL, '0003', '2024-07-12', NULL, 'Absent', NULL, NULL),
+(237, NULL, '0004', '2024-07-12', NULL, 'Present', '07:00:24', NULL),
+(238, NULL, '0001', '2024-07-13', NULL, 'Present', '07:00:24', NULL),
+(239, NULL, '0002', '2024-07-13', NULL, 'Late', '08:00:24', NULL),
+(240, NULL, '0003', '2024-07-13', NULL, 'Present', '07:00:24', NULL),
+(241, NULL, '0004', '2024-07-13', NULL, 'Present', '07:00:24', NULL),
+(242, NULL, '0001', '2024-07-14', NULL, 'Present', '07:00:24', NULL),
+(243, NULL, '0002', '2024-07-14', NULL, 'Present', '07:00:24', NULL),
+(244, NULL, '0003', '2024-07-14', NULL, 'Present', '07:00:24', NULL),
+(245, NULL, '0004', '2024-07-14', NULL, 'Present', '07:00:24', NULL),
+(246, NULL, '0001', '2024-07-15', NULL, 'Present', '07:00:24', NULL),
+(247, NULL, '0002', '2024-07-15', NULL, 'Present', '07:00:24', NULL),
+(248, NULL, '0003', '2024-07-15', NULL, 'Present', '07:00:24', NULL),
+(249, NULL, '0004', '2024-07-15', NULL, 'Present', '07:00:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -191,9 +133,9 @@ CREATE TABLE `branch` (
 --
 
 INSERT INTO `branch` (`branch_ID`, `branch_name`, `established_date`, `street_name`, `barangay`, `city`, `province`, `postal_code`, `contact_number`, `branch_status`) VALUES
-(1, 'Tipolo', '2023-01-24', 'Tipolo Street', 'Maguikay', 'Mandaue', 'Cebu', '6014', '09123456789', 'Active'),
-(3, 'Lapu Lapu', '2020-02-04', '', '', '', '', '', '', 'Active'),
-(14, 'Ibabao', '0000-00-00', '', '', '', '', '', '', 'Active');
+(16, 'Tipolo', '2022-02-02', 'Tipolo', '', 'Mandaue', 'Cebu', '6000', '09423881714', 'Active'),
+(17, 'Lapu Lapu', '2023-03-03', 'Ubos', '', 'Lapu Lapu', 'Cebu', '6000', '09451235472', 'Active'),
+(18, 'Tisa', '2024-05-24', 'Tisa', '', 'Cebu', 'Cebu', '6000', '09527369182', 'Active');
 
 -- --------------------------------------------------------
 
@@ -214,9 +156,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_ID`, `product_name`, `product_description`, `product_price`, `product_status`) VALUES
-(10, 'Liempoer', 'Baboy na Liemp', 300.00, 'Active'),
-(11, 'Spring Rolls', 'Spring Rolled', 10.00, 'Active'),
-(12, 'Chicken', 'Gi luto na Chicken', 270.00, 'Active');
+(38, 'Lechon Manok', 'Grilled Chicken', 270.00, 'Active'),
+(39, 'Liempo', 'Pork Liempo', 280.00, 'Active'),
+(40, 'Lumpia', 'Spring Roll', 10.00, 'Active'),
+(41, 'Fried Chicken', '', 30.00, 'Active'),
+(42, 'Sisig', 'Pork Sisig', 35.00, 'Active');
 
 -- --------------------------------------------------------
 
@@ -247,17 +191,13 @@ CREATE TABLE `salesreport` (
 --
 
 INSERT INTO `salesreport` (`report_ID`, `account_ID`, `branch_ID`, `product_ID`, `report_date`, `report_time`, `cooked_qty`, `reheat_qty`, `total_display_qty`, `left_over_qty`, `total_sold_qty`, `status`, `estimated_revenue`, `remittance`, `pull_out_qty`) VALUES
-(26, '0004', 1, 10, '2024-06-20', '18:15:43', 20, 10, 10, 10, 25, 'Confirmed', 7500.00, NULL, NULL),
-(28, '0005', 3, 10, '2024-07-05', '19:08:23', 15, 5, 5, 5, 15, 'Confirmed', 4500.00, NULL, NULL),
-(104, '0004', 1, 10, '2024-07-08', '22:44:48', 5, 5, 5, 5, 5, 'Confirmed', 1500.00, NULL, NULL),
-(105, '0004', 1, 12, '2024-07-08', '22:44:48', 5, 5, 5, 5, 5, 'Confirmed', 1350.00, NULL, NULL),
-(118, '0004', 1, 10, '2024-07-09', '12:50:14', 5, 5, 5, 5, 5, 'Confirmed', 1500.00, 1500.00, 5),
-(121, '0004', 1, 10, '2024-07-09', '13:30:51', 5, 5, 5, 5, 5, 'Confirmed', 1500.00, 5.00, 5),
-(122, '0004', 1, 11, '2024-07-09', '13:30:51', 5, 5, 5, 5, 5, 'Confirmed', 5.00, 5.00, 55),
-(123, '0004', 14, 10, '2024-07-09', '23:01:52', 5, 5, 5, 5, 5, 'Confirmed', 1500.00, 5.00, 5),
-(124, '0004', 14, 11, '2024-07-09', '23:01:52', 5, 5, 5, 5, 5, 'Confirmed', 50.00, 5.00, 5),
-(132, '0004', 1, 12, '2024-07-12', '17:20:58', 5, 5, 5, 5, 5, 'Confirmed', 5.00, 5.00, 5),
-(133, '0004', 1, 11, '2024-07-15', '12:17:44', 5, 5, 5, 5, 5, 'Confirmed', 50.00, 5.00, 5);
+(134, '0003', 17, 38, '2024-07-16', '12:06:25', 5, 5, 5, 5, 5, 'Confirmed', 1350.00, 1350.00, 5),
+(135, '0003', 17, 39, '2024-07-16', '12:06:25', 5, 5, 5, 5, 5, 'Confirmed', 500.00, 500.00, 5),
+(136, '0003', 17, 41, '2024-07-16', '12:18:02', 5, 5, 5, 5, 5, 'Confirmed', 150.00, 150.00, 5),
+(137, '0004', 18, 41, '2024-07-11', '13:08:28', 10, 8, 18, 14, 4, 'Pending', NULL, 120.00, 0),
+(138, '0004', 18, 38, '2024-07-11', '13:09:43', 2, 3, 5, 2, 3, 'Pending', NULL, 810.00, 0),
+(139, '0004', 18, 40, '2024-07-11', '13:10:28', 10, 10, 20, 20, 0, 'Pending', NULL, 0.00, 0),
+(140, '0003', 17, 38, '2024-07-15', '13:47:49', 5, 5, 5, 5, 5, 'Pending', NULL, 500.00, 5);
 
 -- --------------------------------------------------------
 
@@ -294,11 +234,10 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`staff_ID`, `last_name`, `first_name`, `middle_name`, `house_number`, `street_name`, `barangay`, `city`, `province`, `postal_code`, `birth_date`, `gender`, `contact_1`, `contact_2`, `email`, `SSN`, `TIN`, `position_title`, `start_date`, `salary`, `status`) VALUES
-('0001', 'Dabon', 'Dino Cyrano', 'Azucenas', '628', 'M.L. Quezon', 'Maguikay', 'Mandaue', 'Cebu', '6014', '2000-09-24', 'Male', '09955430188', '09323995999', 'dcdabon1231@gmail.com', '', '', 'Production', '2024-06-28', 528.00, 'Active'),
-('0002', 'Test', 'Testing', 'Test', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active'),
-('0003', 'test', 'test', 'test', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active'),
-('0004', 'testing', 'testing', 'testing', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active'),
-('0005', 'dab', 'dab', 'd', '', '', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '', '0000-00-00', 0.00, 'Active');
+('0001', 'Dabon', 'Dino Cyrano', 'Azucenas', '628', 'M.L. Quezon Street', 'Maguikay', 'Mandaue', 'Cebu', '6014', '2000-09-24', 'Male', '09955430188', '09323995999', 'dcdabon1231@gmail.com', '', '', 'Manager', '2024-07-16', 528.00, 'Active'),
+('0002', 'Ramo', 'Justin', '', '257', 'Cabancalan', 'Porok 1', 'Mandaue', 'Cebu', '6000', '2004-01-01', 'Male', '09123456789', '', 'justinramo@gmail.com', '', '', 'Production', '2024-07-16', 528.00, 'Active'),
+('0003', 'Linao', 'Carl Andrew', '', '512', 'Street Name', 'Barangay', 'Lapu Lapu', 'Cebu', '6000', '2004-02-02', 'Male', '09121231234', '', 'carlandrew@gmail.com', '', '', 'Staff', '2024-02-02', 528.00, 'Active'),
+('0004', 'Calihog', 'Kenneth', '', '582', 'Street Name', 'Barangay', 'City', 'Province', '6000', '2004-07-05', 'Male', '09258271925', '', 'kennethcalihog@gmail.com', '', '', 'Staff', '2024-07-16', 528.00, 'Active');
 
 --
 -- Indexes for dumped tables
@@ -353,7 +292,7 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `assignment`
 --
 ALTER TABLE `assignment`
-  MODIFY `assignment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=184;
+  MODIFY `assignment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=250;
 
 --
 -- AUTO_INCREMENT for table `branch`
@@ -371,7 +310,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `salesreport`
 --
 ALTER TABLE `salesreport`
-  MODIFY `report_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
+  MODIFY `report_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
 
 --
 -- Constraints for dumped tables
