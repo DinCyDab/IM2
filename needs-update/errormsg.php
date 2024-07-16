ac7c8c7d3f2fae477a10a1a3719b5760e6f5e7e8
Munckenh <76769232+Munckenh@users.noreply.github.com>
2024-07-16 19:02:51 +0800

    Delete `needs-update/` folder

--- needs-update/errormsg.php
+++
@@ -1,6 +0,0 @@
-<div class="errorMsgHolder" id="errorMsg">
-    <div class="errorMSG" id="errorMsg1">
-        <h1>Failed to Submit Report</h1>
-        <p>You are not assigned to any Branch</p>
-    </div>
-</div>
--- needs-update/navstaff.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="gsr.php">Generate Sales Report</a></li>
-        <li><a href="schedule.php">Schedule</a></li>
-        <li><a href="staffpendingreports.php">Pending Reports</a></li>
-        <li><a href="editprofile.php">Account Settings</a></li>
-        <li><a href="logout.php">Log Out</a></li>
-    </ul>
-</nav>
