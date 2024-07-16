547394360d728d035adc1766e17f2db272883686
Munckenh <76769232+Munckenh@users.noreply.github.com>
2024-07-16 21:19:24 +0800

    Add notification button, add hover effect on logout button

--- admin.php
+++ admin.php
@@ -86,7 +86,7 @@ setDefaultDate();
         </ul>
         <div class="account-info">
             <span><?php echo $_SESSION["last_name"] . " (" . $_SESSION["role"] . ")" ?></span>
-            <a href="?logout">
+            <a href="?logout" class="action-button">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                     <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
                 </svg>
@@ -96,7 +96,11 @@ setDefaultDate();
     <div class="content">
         <div class="content-header">
             <h1>Dashboard</h1>
-            <button class="notifications"></button>
+            <button class="action-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FfFFFF">
+                    <path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z" />
+                </svg>
+            </button>
         </div>
         <div class="content-actions"></div>
         <div class="dashboard"></div>
